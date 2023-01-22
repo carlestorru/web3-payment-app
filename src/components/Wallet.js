be5180039328c33b2053a617db99a63e6746b37a
carlestorru <carlestorru@gmail.com>
2023-01-22 13:02:27 +0100

    format document

--- src/components/Wallet.js
+++ src/components/Wallet.js
@@ -2,20 +2,22 @@ import { useWeb3React } from "@web3-react/core";
 import { useEffect, useState } from "react";
 
 export function Wallet() {
-    const { chainId, account, library } = useWeb3React();
-    const [balance, setBalance] = useState('');
+  const { chainId, account, library } = useWeb3React();
+  const [balance, setBalance] = useState('');
 
-    const web3 = library;
-    
-    useEffect(() => {
-        web3.eth.getBalance(account).then((result) => setBalance(web3.utils.fromWei(result)));
-    }, [account, web3])
+  const web3 = library;
 
-    return (
-        <div>
-            <p>Chain Id: {chainId}</p>
-            <p>Account: {account}</p>
-            <p>Balance: {balance}</p>
-        </div>
-    )
+  useEffect(() => {
+    web3.eth
+      .getBalance(account)
+      .then((result) => setBalance(web3.utils.fromWei(result)));
+  }, [account, web3]);
+
+  return (
+    <div>
+      <p>Chain Id: {chainId}</p>
+      <p>Account: {account}</p>
+      <p>Balance: {balance}</p>
+    </div>
+  );
 }
