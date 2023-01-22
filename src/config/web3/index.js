5c8900a31bd59f3835ebfce4b82fb537cbb5fb71
carlestorru <carlestorru@gmail.com>
2023-01-22 12:43:39 +0100

    web3 config connector and library

---
+++ src/config/web3/index.js
@@ -0,0 +1,11 @@
+import web3 from 'web3';
+import { InjectedConnector } from '@web3-react/injected-connector';
+
+export const connector = new InjectedConnector({
+    supportedChainIds: [1, 3, 4, 5, 42, 1337]
+});
+
+export const getLibrary = (provider) => {
+    const library = new web3(provider);
+    return library;
+}
