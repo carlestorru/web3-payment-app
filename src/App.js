e2dd7ec3cc6abbe92aac9862af19022ebc7490e6
carlestorru <carlestorru@gmail.com>
2023-01-23 08:58:26 +0100

    format documents with prettier

--- src/App.js
+++ src/App.js
@@ -1,45 +1,45 @@
 import './App.css';
 import { useEffect, useCallback, useState } from 'react';
-import { useWeb3React } from '@web3-react/core'; 
-import { connector } from './config/web3'
+import { useWeb3React } from '@web3-react/core';
+import { connector } from './config/web3';
 
 import { Wallet } from './components/Wallet';
 
 function App() {
-  const { activate, active, deactivate, error } = useWeb3React();
+	const { activate, active, deactivate, error } = useWeb3React();
 
-  const connect = useCallback(() => {
-    activate(connector);
-    localStorage.setItem('previouslyConnected', true);
-  }, [activate]);
+	const connect = useCallback(() => {
+		activate(connector);
+		localStorage.setItem('previouslyConnected', true);
+	}, [activate]);
 
-  useEffect(() => {
-    if (localStorage.getItem('previouslyConnected') === 'true') {
-      connect()
-    }
-  }, [connect]);
+	useEffect(() => {
+		if (localStorage.getItem('previouslyConnected') === 'true') {
+			connect();
+		}
+	}, [connect]);
 
-  const disconnect = () => {
-    deactivate();
-    localStorage.removeItem('previouslyConnected');
-  };
+	const disconnect = () => {
+		deactivate();
+		localStorage.removeItem('previouslyConnected');
+	};
 
-  if (error) {
-    console.error(error);
-  }
+	if (error) {
+		console.error(error);
+	}
 
-  return (
-    <main>
-      {
-        active 
-        ? <>
-            <button onClick={disconnect}>Desconectar Wallet</button>
-            <Wallet />
-          </>
-        : <button onClick={connect}>Conectar wallet</button>
-      }
-    </main>
-  );
+	return (
+		<main>
+			{active ? (
+				<>
+					<button onClick={disconnect}>Desconectar Wallet</button>
+					<Wallet />
+				</>
+			) : (
+				<button onClick={connect}>Conectar wallet</button>
+			)}
+		</main>
+	);
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -3,14 +3,14 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 
-import { getLibrary } from './config/web3'
+import { getLibrary } from './config/web3';
 import { Web3ReactProvider } from '@web3-react/core';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <Web3ReactProvider getLibrary={getLibrary}>
-      <App />
-    </Web3ReactProvider>
-  </React.StrictMode>
+	<React.StrictMode>
+		<Web3ReactProvider getLibrary={getLibrary}>
+			<App />
+		</Web3ReactProvider>
+	</React.StrictMode>
 );
