3af2a046762295d6583d215e620c60ca1ad8f01d
carlestorru <carlestorru@gmail.com>
2023-01-29 13:00:47 +0100

    add to component

---
+++ src/components/WalletConnector.jsx
@@ -0,0 +1,49 @@
+import { useEffect, useCallback } from 'react';
+import { useWeb3React, UnsupportedChainIdError } from '@web3-react/core';
+import { connector } from '../config/web3';
+import {
+	NoEthereumProviderError,
+	UserRejectedRequestError,
+} from '@web3-react/injected-connector';
+
+import { Wallet } from './WalletCard';
+
+export function WalletConnector() {
+	const { activate, active, deactivate, error } = useWeb3React();
+
+	const connect = useCallback(() => {
+		activate(connector);
+		localStorage.setItem('previouslyConnected', true);
+	}, [activate]);
+
+	useEffect(() => {
+		if (localStorage.getItem('previouslyConnected') === 'true') {
+			connect();
+		}
+	}, [connect]);
+
+	const disconnect = () => {
+		deactivate();
+		localStorage.removeItem('previouslyConnected');
+	};
+
+	const isNoEthereumProviderError = error instanceof NoEthereumProviderError;
+	const isUserRejectedRequestError = error instanceof UserRejectedRequestError;
+	const isUnsupportedChainIdError = error instanceof UnsupportedChainIdError;
+	console.log(isNoEthereumProviderError);
+	console.log(isUserRejectedRequestError);
+	console.log(isUnsupportedChainIdError);
+
+	return (
+		<main>
+			{active ? (
+				<>
+					<button onClick={disconnect}>Desconectar Wallet</button>
+					<Wallet />
+				</>
+			) : (
+				<button onClick={connect}>Conectar wallet</button>
+			)}
+		</main>
+	);
+}
