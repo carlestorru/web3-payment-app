799ef037bf142448d8ec09bcb162e154fe21e177
carlestorru <carlestorru@gmail.com>
2023-02-04 11:15:22 +0100

    add new routes

--- src/routes/MainRoutes.js
+++ src/routes/MainRoutes.js
@@ -1,6 +1,7 @@
-import { lazy } from "react";
+import { lazy } from 'react';
 
-import Loadable from "../components/Loadable";
+import Loadable from '../components/Loadable';
+import MainLayout from '../layout/MainLayout';
 
 const Landing = Loadable(lazy(() => import('../pages/Landing/Landing')));
 const Panel = Loadable(lazy(() => import('../pages/Panel/Panel')));
@@ -9,26 +10,40 @@ const Wallet = Loadable(lazy(() => import('../pages/Wallet/Wallet')));
 const Activity = Loadable(lazy(() => import('../pages/Activity/Activity')));
 
 const MainRoutes = [
-    {
-        path: '/',
-        element: <Landing />,
-    },
-    {
-        path: '/panel',
-        element: <Panel /> 
-    },
-    {
-        path: '/transfer',
-        element: <Transfer /> 
-    },
-    {
-        path: '/wallet',
-        element: <Wallet /> 
-    },
-    {
-        path: '/activity',
-        element: <Activity /> 
-    },
-]
+	{
+		path: '/landing',
+		element: <Landing />,
+	},
+	{
+		path: '/',
+		element: <MainLayout />,
+		children: [
+			{
+				path: '/',
+				element: <Panel />,
+			},
+			{
+				path: '/panel',
+				element: <Panel />,
+			},
+			{
+				path: '/transfer',
+				element: <Transfer />,
+			},
+			{
+				path: '/wallet',
+				element: <Wallet />,
+			},
+			{
+				path: '/activity',
+				element: <Activity />,
+			},
+		],
+	},
+	{
+		path: '*',
+		element: <h1>404 NOT FOUND</h1>,
+	},
+];
 
 export default MainRoutes;
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,7 +1,7 @@
-import { useRoutes } from "react-router-dom";
+import { useRoutes } from 'react-router-dom';
 
 import MainRoutes from './MainRoutes';
 
 export default function Routes() {
-    return useRoutes(MainRoutes);
+	return useRoutes(MainRoutes);
 }
