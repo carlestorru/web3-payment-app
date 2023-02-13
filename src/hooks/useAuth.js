d4db514802a4400bbe80853580284c9cdedbfbc2
carlestorru <carlestorru@gmail.com>
2023-02-13 18:02:22 +0100

    useAuth hook

---
+++ src/hooks/useAuth.js
@@ -0,0 +1,23 @@
+import { useEffect, useCallback } from 'react';
+import { useWeb3React } from '@web3-react/core';
+import { connector } from '../config/web3';
+import { useLocation, useNavigate } from 'react-router-dom';
+
+export default function useAuth() {
+	const { activate } = useWeb3React();
+	const navigate = useNavigate();
+	const location = useLocation();
+
+	const connect = useCallback(() => {
+		activate(connector);
+		localStorage.setItem('previouslyConnected', true);
+	}, [activate]);
+
+	useEffect(() => {
+		if (localStorage.getItem('previouslyConnected') === 'true') {
+			connect();
+		} else {
+			navigate('/landing', { replace: true, state: { from: location } });
+		}
+	}, [connect]);
+}
