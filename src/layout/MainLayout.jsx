9f3e23fbaaee9f84d542919735f53b9399738b98
carlestorru <carlestorru@gmail.com>
2023-02-13 17:34:37 +0100

    ui changes

--- src/layout/MainLayout.jsx
+++ src/layout/MainLayout.jsx
@@ -4,7 +4,7 @@ import Ethereum from '../components/Icons/Ethereum';
 import Menu from '../components/Icons/Menu';
 import menuItems from '../menu-items';
 
-function MainLayout () {
+function MainLayout() {
 	const [showMenu, setShowMenu] = useState(true);
 	const sidebar = useRef();
 	const toggleMenu = () => {
@@ -15,14 +15,14 @@ function MainLayout () {
 	return (
 		<div className='relative min-h-screen md:flex'>
 			{/* Mobile menu bar */}
-			<header className='flex justify-between bg-gray-800 text-gray-100 md:hidden'>
+			<header className='flex items-center justify-between bg-white text-gray-600 md:hidden'>
 				{/* Logo */}
 				<span className='text-2xl font-extrabold'>Ethereum</span>
 
 				<button
-					className='p-4 focus:bg-gray-700 focus:outline-none'
+					className='p-4 focus:bg-blue-300 focus:outline-none'
 					onClick={toggleMenu}>
-					<Menu color='#FFF' />
+					<Menu color='#4b5563' />
 				</button>
 			</header>
 
@@ -30,7 +30,7 @@ function MainLayout () {
 			<div
 				ref={sidebar}
 				className={
-					'absolute inset-y-0 left-0 w-64 -translate-x-full transform space-y-6 bg-blue-800 py-7 px-2 text-blue-100 transition duration-200 ease-in-out md:relative md:translate-x-0'
+					'absolute inset-y-0 left-0 w-64 -translate-x-full transform space-y-6 bg-white py-7 px-2 text-gray-600 transition duration-200 ease-in-out md:relative md:translate-x-0'
 				}>
 				{/* Logo */}
 				<Ethereum />
@@ -39,35 +39,40 @@ function MainLayout () {
 					{menuItems.map((item) => {
 						return (
 							<ul key={item.id} className='block py-2.5 px-2'>
-								<h3 className='font-bold text-white'>{item.title}</h3>
-								{item.children.map((child) => {
-									return (
-										<li
-											key={child.id}
-											className='rounded transition hover:bg-blue-700'>
-											<NavLink
-												className={({ isActive }) => {
-													return isActive
-														? 'list-item rounded bg-red-500 py-2.5 px-4 font-semibold'
-														: 'list-item rounded py-2.5 px-4';
-												}}
-												to={child.url}>
-												{child.title}
-											</NavLink>
-										</li>
-									);
-								})}
+								<li>
+									<span className='uppercase font-bold'>{item.title}</span>
+									<ul>
+										{item.children.map((child) => {
+											return (
+												<li key={child.id} className='py-0.5'>
+													<NavLink
+														className={({ isActive }) => {
+															return isActive
+																? 'list-item rounded bg-blue-300 font-semibold text-black'
+																: 'list-item rounded transition hover:bg-blue-200';
+														}}
+														to={child.url}>
+														<div className='flex flex-row items-center justify-start gap-4 py-2.5 px-1'>
+															{child.icon}
+															{child.title}
+														</div>
+													</NavLink>
+												</li>
+											);
+										})}
+									</ul>
+								</li>
 							</ul>
 						);
 					})}
 				</nav>
 			</div>
 			{/* Content */}
-			<main className='flex-1 p-10 text-2xl font-bold'>
+			<main className='min-h-screen flex-1 bg-slate-100 p-10 text-2xl font-bold'>
 				<Outlet />
 			</main>
 		</div>
 	);
-};
+}
 
 export default MainLayout;
