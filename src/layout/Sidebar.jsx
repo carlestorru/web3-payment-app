4a343c581daca98516f32164bf46de053e0673ab
carlestorru <carlestorru@gmail.com>
2023-02-20 17:31:19 +0100

    isLinkActive function

--- src/layout/Sidebar.jsx
+++ src/layout/Sidebar.jsx
@@ -2,10 +2,15 @@ import Ethereum from '../components/Icons/Ethereum';
 import menuItems from '../menu-items';
 import { useLayoutMode } from '../context/LayoutContext';
 
-import { NavLink } from 'react-router-dom';
+import { NavLink, useLocation } from 'react-router-dom';
 
 export default function SideBar() {
 	const [isOpen] = useLayoutMode();
+
+	const isLinkActive = (url) => {
+		const location = useLocation();
+		return location.pathname === url;
+	};
 
 	return (
 		<div
@@ -28,14 +33,23 @@ export default function SideBar() {
 												<NavLink
 													className={({ isActive }) => {
 														return isActive
-															? 'list-item rounded bg-blue-300 font-semibold text-black'
+															? 'list-item rounded bg-blue-300 font-medium text-black'
 															: 'list-item rounded transition hover:bg-blue-200';
 													}}
 													to={child.url}>
-													<div className='flex flex-row items-center justify-start gap-4 py-2 px-1'>
-														{child.icon}
-														{child.title}
-													</div>
+													{() =>
+														isLinkActive(child.url) ? (
+															<div className='flex flex-row items-center justify-start gap-4 py-2 px-1 text-blue-800'>
+																{child.icon}
+																{child.title}
+															</div>
+														) : (
+															<div className='flex flex-row items-center justify-start gap-4 py-2 px-1'>
+																{child.icon}
+																{child.title}
+															</div>
+														)
+													}
 												</NavLink>
 											</li>
 										);
