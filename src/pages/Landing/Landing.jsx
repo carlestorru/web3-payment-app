2fd383ac359cd41b319fab3c5c313c76833538fa
carlestorru <carlestorru@gmail.com>
2023-02-22 18:48:54 +0100

    landing page style

--- src/pages/Landing/Landing.jsx
+++ src/pages/Landing/Landing.jsx
@@ -1,12 +1,20 @@
 import { WalletConnector } from '../../components/WalletConnector';
 import useDocumentTitle from '../../hooks/useDocumentTitle';
 
+import styles from './Landing.module.css';
+
 const Landing = () => {
 	useDocumentTitle('Landing');
 
 	return (
-		<main className='flex h-screen flex-col items-center justify-center'>
-			<h1>Landing page</h1>
+		<main
+			className={`${styles.main} p-5 flex h-screen w-full flex-col items-center justify-center gap-12 text-center`}>
+			<h1 className='text-7xl font-bold'>Web3 Payment App</h1>
+			<h2 className='text-xl font-medium'>
+				Paga tus compras en Ecommerce, envia dinero a tus contactos, emite
+				facturas, consulta tu historial de transaciones y mucho más de forma{' '}
+				<span className='text-transparent bg-clip-text bg-gradient-to-br from-blue-400 to-blue-900'>descentralizada</span>
+			</h2>
 			<WalletConnector />
 		</main>
 	);
