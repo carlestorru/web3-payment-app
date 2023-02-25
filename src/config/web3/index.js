9129b5516527bc8f0baea9e1c25bddcefad685ab
carlestorru <carlestorru@gmail.com>
2023-02-25 11:46:40 +0100

    allow only 1337 chain id

--- src/config/web3/index.js
+++ src/config/web3/index.js
@@ -2,7 +2,8 @@ import Web3 from 'web3';
 import { InjectedConnector } from '@web3-react/injected-connector';
 
 export const connector = new InjectedConnector({
-	supportedChainIds: [1, 3, 4, 5, 42, 1337],
+	// 1, 3, 4, 5, 42
+	supportedChainIds: [1337],
 });
 
 export const getLibrary = (provider) => {
