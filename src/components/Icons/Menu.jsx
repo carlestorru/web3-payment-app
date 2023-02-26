0bec58f832f51c5192b6744d1a1cf6db06a13876
carlestorru <carlestorru@gmail.com>
2023-02-26 12:38:26 +0100

    change color prop for currentColor

--- src/components/Icons/Menu.jsx
+++ src/components/Icons/Menu.jsx
@@ -1,11 +1,11 @@
-export default function Menu({ color }) {
+export default function Menu() {
 	return (
 		<svg
 			xmlns='http://www.w3.org/2000/svg'
 			fill='none'
 			viewBox='0 0 24 24'
 			strokeWidth={1.5}
-			stroke={color}
+			stroke='currentColor'
 			className='h-6 w-6'>
 			<path
 				strokeLinecap='round'
