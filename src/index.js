595381ce708ead9e1bc262c243ea317a4642f4ff
carlestorru <carlestorru@gmail.com>
2023-03-06 19:18:13 +0100

    react-query package

--- src/index.js
+++ src/index.js
@@ -5,12 +5,17 @@ import App from './App';
 import { getLibrary } from './config/web3';
 import { Web3ReactProvider } from '@web3-react/core';
 import { BrowserRouter } from 'react-router-dom';
+import { QueryClient, QueryClientProvider } from 'react-query';
+
+const queryClient = new QueryClient();
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
 	<Web3ReactProvider getLibrary={getLibrary}>
-		<BrowserRouter>
-			<App />
-		</BrowserRouter>
+		<QueryClientProvider client={queryClient}>
+			<BrowserRouter>
+				<App />
+			</BrowserRouter>
+		</QueryClientProvider>
 	</Web3ReactProvider>
 );
