92830ebb6e95eb56238d39f80ed585f756c41cb0
carlestorru <carlestorru@gmail.com>
2023-03-10 18:46:35 +0100

    add two new tipes of arrows

--- src/components/Icons/Outlined/Arrow.jsx
+++ src/components/Icons/Outlined/Arrow.jsx
@@ -33,3 +33,39 @@ export function ArrowUp() {
 		</svg>
 	);
 }
+
+export function ArrowUpRight() {
+	return (
+		<svg
+			fill='none'
+			stroke='currentColor'
+			strokeWidth={1.5}
+			viewBox='0 0 24 24'
+			xmlns='http://www.w3.org/2000/svg'
+			aria-hidden='true'>
+			<path
+				strokeLinecap='round'
+				strokeLinejoin='round'
+				d='M4.5 19.5l15-15m0 0H8.25m11.25 0v11.25'
+			/>
+		</svg>
+	);
+}
+
+export function ArrowDownLeft() {
+	return (
+		<svg
+			fill='none'
+			stroke='currentColor'
+			strokeWidth={1.5}
+			viewBox='0 0 24 24'
+			xmlns='http://www.w3.org/2000/svg'
+			aria-hidden='true'>
+			<path
+				strokeLinecap='round'
+				strokeLinejoin='round'
+				d='M19.5 4.5l-15 15m0 0h11.25m-11.25 0V8.25'
+			/>
+		</svg>
+	);
+}
