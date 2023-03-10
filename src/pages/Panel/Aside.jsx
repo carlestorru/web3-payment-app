3cd7939bd4764c4440f51e7cef511af2b43f4060
carlestorru <carlestorru@gmail.com>
2023-03-10 18:47:37 +0100

    aside style

--- src/pages/Panel/Aside.jsx
+++ src/pages/Panel/Aside.jsx
@@ -1,5 +1,26 @@
+import {
+	ArrowDownLeft,
+	ArrowUpRight,
+} from '../../components/Icons/Outlined/Arrow';
+
 export function Aside() {
 	return (
-		<aside className='col-start-4 row-span-2 row-start-1  bg-red-500'></aside>
+		<aside className='col-start-8 row-span-2 row-start-1 flex flex-col justify-start items-center max-sm:hidden'>
+			<section className='flex flex-col justify-center gap-4'>
+				<h4></h4>
+				<article className='flex flex-col items-center'>
+					<button className='h-12 w-12 rounded-full bg-blue-500 hover:bg-opacity-90 p-3 text-white'>
+						<ArrowUpRight />
+					</button>
+					<h5 className='text-center text-black text-sm font-medium'>Enviar</h5>
+				</article>
+				<article className='flex flex-col items-center'>
+					<button className='h-12 w-12 rounded-full bg-blue-500 hover:bg-opacity-90 p-3 text-white'>
+						<ArrowDownLeft />
+					</button>
+					<h5 className='text-center text-black text-sm font-medium'>Solicitar</h5>
+				</article>
+			</section>
+		</aside>
 	);
 }
