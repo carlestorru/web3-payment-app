f254d3e4ca04161850dc375f9abeaa9dd62f7f87
carlestorru <carlestorru@gmail.com>
2023-03-15 19:22:46 +0100

    add date and time to tx

--- src/services/getTransactionsByAccount.js
+++ src/services/getTransactionsByAccount.js
@@ -5,17 +5,27 @@ export default async function getTransacctionsByAccount(account, web3) {
 
 	for (let blockNum = 0; blockNum <= latestBlock.number; blockNum++) {
 		const block = await web3.eth.getBlock(blockNum);
+		const blockDate = new Date(block.timestamp * 1000);
 		if (block !== null && block.transactions !== null) {
 			for (const txHash of block.transactions) {
 				const tx = await web3.eth.getTransaction(txHash);
 				if (account === tx.to) {
-					to.push(tx);
+					to.push({
+						...tx,
+						date: blockDate.toLocaleDateString(),
+						time: blockDate.toLocaleTimeString(),
+					});
 				}
 				if (account === tx.from) {
-					from.push(tx);
+					from.push({
+						...tx,
+						date: blockDate.toLocaleDateString(),
+						time: blockDate.toLocaleTimeString(),
+					});
 				}
 			}
 		}
+		console.log(from);
 	}
 
 	return { from, to };
