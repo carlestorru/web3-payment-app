6c0a35b35d844964539d52602335592ed392d2d9
carlestorru <carlestorru@gmail.com>
2023-03-21 16:54:50 +0100

    move particles outsite main tag

--- src/pages/Landing/Landing.jsx
+++ src/pages/Landing/Landing.jsx
@@ -23,118 +23,117 @@ const Landing = () => {
 
 	return (
 		<>
+			<Particles
+				id='tsparticles'
+				init={particlesInit}
+				loaded={particlesLoaded}
+				options={{
+					particles: {
+						number: {
+							value: 80,
+							density: {
+								enable: true,
+								value_area: 800,
+							},
+						},
+						color: {
+							value: '#ffffff',
+						},
+						shape: {
+							type: 'circle',
+							stroke: {
+								width: 0,
+								color: '#000000',
+							},
+							polygon: {
+								nb_sides: 5,
+							},
+						},
+						opacity: {
+							value: 0.5,
+							random: false,
+							anim: {
+								enable: false,
+								speed: 1,
+								opacity_min: 0.1,
+								sync: false,
+							},
+						},
+						size: {
+							value: 3,
+							random: true,
+							anim: {
+								enable: false,
+								speed: 40,
+								size_min: 0.1,
+								sync: false,
+							},
+						},
+						line_linked: {
+							enable: true,
+							distance: 150,
+							color: '#ffffff',
+							opacity: 0.4,
+							width: 1,
+						},
+						move: {
+							enable: true,
+							speed: 6,
+							direction: 'none',
+							random: false,
+							straight: false,
+							out_mode: 'out',
+							bounce: false,
+							attract: {
+								enable: false,
+								rotateX: 600,
+								rotateY: 1200,
+							},
+						},
+					},
+					interactivity: {
+						detect_on: 'canvas',
+						events: {
+							onhover: {
+								enable: false,
+								mode: 'repulse',
+							},
+							onclick: {
+								enable: false,
+								mode: 'push',
+							},
+							resize: true,
+						},
+						modes: {
+							grab: {
+								distance: 400,
+								line_linked: {
+									opacity: 1,
+								},
+							},
+							bubble: {
+								distance: 400,
+								size: 40,
+								duration: 2,
+								opacity: 8,
+								speed: 3,
+							},
+							repulse: {
+								distance: 200,
+								duration: 0.4,
+							},
+							push: {
+								particles_nb: 4,
+							},
+							remove: {
+								particles_nb: 2,
+							},
+						},
+					},
+					retina_detect: true,
+				}}
+			/>
 			<main className='flex h-screen w-full flex-col justify-evenly bg-blue-600 p-5 text-center text-white'>
-				<Particles
-					id='tsparticles'
-					init={particlesInit}
-					loaded={particlesLoaded}
-					options={{
-						particles: {
-							number: {
-								value: 80,
-								density: {
-									enable: true,
-									value_area: 800,
-								},
-							},
-							color: {
-								value: '#ffffff',
-							},
-							shape: {
-								type: 'circle',
-								stroke: {
-									width: 0,
-									color: '#000000',
-								},
-								polygon: {
-									nb_sides: 5,
-								},
-							},
-							opacity: {
-								value: 0.5,
-								random: false,
-								anim: {
-									enable: false,
-									speed: 1,
-									opacity_min: 0.1,
-									sync: false,
-								},
-							},
-							size: {
-								value: 3,
-								random: true,
-								anim: {
-									enable: false,
-									speed: 40,
-									size_min: 0.1,
-									sync: false,
-								},
-							},
-							line_linked: {
-								enable: true,
-								distance: 150,
-								color: '#ffffff',
-								opacity: 0.4,
-								width: 1,
-							},
-							move: {
-								enable: true,
-								speed: 6,
-								direction: 'none',
-								random: false,
-								straight: false,
-								out_mode: 'out',
-								bounce: false,
-								attract: {
-									enable: false,
-									rotateX: 600,
-									rotateY: 1200,
-								},
-							},
-						},
-						interactivity: {
-							detect_on: 'canvas',
-							events: {
-								onhover: {
-									enable: false,
-									mode: 'repulse',
-								},
-								onclick: {
-									enable: false,
-									mode: 'push',
-								},
-								resize: true,
-							},
-							modes: {
-								grab: {
-									distance: 400,
-									line_linked: {
-										opacity: 1,
-									},
-								},
-								bubble: {
-									distance: 400,
-									size: 40,
-									duration: 2,
-									opacity: 8,
-									speed: 3,
-								},
-								repulse: {
-									distance: 200,
-									duration: 0.4,
-								},
-								push: {
-									particles_nb: 4,
-								},
-								remove: {
-									particles_nb: 2,
-								},
-							},
-						},
-						retina_detect: true,
-					}}
-				/>
-
 				<h1 className='w-[80%] self-center rounded-lg bg-white py-3 text-7xl font-bold text-blue-500'>
 					3Pay
 				</h1>
