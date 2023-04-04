aec71aaa951c257574c698c69a5cdfb6fbd7ab8d
carlestorru <carlestorru@gmail.com>
2023-04-04 18:02:26 +0200

    new transaction schema

--- api/src/models/Transaction.js
+++ api/src/models/Transaction.js
@@ -1,10 +1,24 @@
 const mongoose = require('mongoose');
 
 const TransactionSchema = new mongoose.Schema({
-    hash: {
-        type: String,
-        required: true,
-    }
+    blockHash: String,
+    blockNumber: Number,
+    from: String,
+    gas: Number,
+    gasPrice: String,
+    hash: String,
+    input: String,
+    nonce: Number,
+    r: String,
+    s: String,
+    to: String,
+    transactionIndex: Number,
+    type: Number,
+    v: String,
+    value: String
+}, {
+    versionKey: false,
+    timestamps: {createdAt: true, updatedAt: false}
 })
 
 const Transaction = mongoose.model("Transaction", TransactionSchema);
