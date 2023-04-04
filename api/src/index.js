43f3cf6be90454a1735929a59c0e7fc8d0826eb4
carlestorru <carlestorru@gmail.com>
2023-04-04 13:40:06 +0200

    connection db

--- api/src/index.js
+++ api/src/index.js
@@ -1,13 +1,29 @@
+require('dotenv').config();
+
 const express = require('express');
+const mongoose = require('mongoose');
 
 const v1AppRouter = require('./v1/routes/appRoutes');
 
-const app = express();
 const PORT = process.env.PORT || 3001;
+const mongoString = process.env.DATABASE_URL;
 
-app.use(express.json());
-app.use('/api/v1', v1AppRouter);
+const app = express();
 
-app.listen(PORT, () => {
-	console.log(`API is listening on port ${PORT}`);
-});
+async function start() {
+	mongoose.connect(mongoString);
+	try {
+		await mongoose.connect(mongoString);
+		console.log('Connected to DB')
+		app.use(express.json());
+		app.use('/api/v1', v1AppRouter);
+
+		app.listen(PORT, () => {
+			console.log(`API is listening on port ${PORT}`);
+		});
+	} catch (error) {
+		console.error(err);
+	}
+}
+
+start();
