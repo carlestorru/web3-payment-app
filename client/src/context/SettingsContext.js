e245f28eff7dc8cc56f1cb53a6e04458c313c76e
carlestorru <carlestorru@gmail.com>
2023-04-10 19:14:14 +0200

    dark mode settings

--- client/src/context/SettingsContext.js
+++ client/src/context/SettingsContext.js
@@ -4,11 +4,41 @@ const SettingsContext = createContext();
 
 export function SettingsProvider({ children }) {
 	const [settings, setSettings] = useState({
-        readDB: true
-    });
+		readDB: true,
+		darkMode: false,
+	});
+
+	const toggleDataDB = () => {
+		setSettings((prev) => {
+			return { ...prev, readDB: !prev.readDB };
+		});
+	};
+
+	const toggleDarkMode = () => {
+		if (settings.darkMode) {
+			document.documentElement.classList.remove('dark');
+		} else {
+			document.documentElement.classList.add('dark');
+		}
+		localStorage.setItem('darkMode', !settings.darkMode);
+		setSettings((prev) => {
+			return { ...prev, darkMode: !prev.darkMode };
+		});
+	};
+
+	const checkIfDarkMode = () => {
+		if (localStorage.getItem('darkMode')) {
+			document.documentElement.classList.add('dark');
+		} else {
+			document.documentElement.classList.remove('dark');
+		}
+		setSettings(prev => {
+			return { ...prev, darkMode: localStorage.getItem('darkMode')}
+		})
+	};
 
 	return (
-		<SettingsContext.Provider value={[settings, setSettings]}>
+		<SettingsContext.Provider value={[settings, toggleDataDB, toggleDarkMode, checkIfDarkMode]}>
 			{children}
 		</SettingsContext.Provider>
 	);
