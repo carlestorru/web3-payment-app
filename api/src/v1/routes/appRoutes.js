848b045e3913f07bf86da70cb3e12c945b1f9e33
carlestorru <carlestorru@gmail.com>
2023-04-10 20:01:03 +0200

    root path

--- api/src/v1/routes/appRoutes.js
+++ api/src/v1/routes/appRoutes.js
@@ -4,6 +4,10 @@ const accountsController = require('../../controllers/accountsController.js');
 
 const router = express.Router();
 
+router.get('/', (req, res) => {
+	res.send('This is v1 of 3pay API');
+});
+
 router.get('/transactions', transactionController.getAllTransactions);
 router.get('/transactions/:hash', transactionController.getTransaction);
 router.post('/transactions', transactionController.saveTransaction);
