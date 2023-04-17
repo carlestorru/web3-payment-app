be8f33d6d7d00a686dc3a5a8801a02610883e42f
carlestorru <carlestorru@gmail.com>
2023-04-17 17:16:54 +0200

    check if tx.to is not null

--- client/src/services/getTransactions.js
+++ client/src/services/getTransactions.js
@@ -21,7 +21,7 @@ async function getTransactionsFromBlockchain(account, web3) {
 		if (block !== null && block.transactions !== null) {
 			for (const txHash of block.transactions) {
 				const tx = await web3.eth.getTransaction(txHash);
-				if (account === tx.from || account === tx.to) {
+				if (tx.to !== null && (account === tx.from || account === tx.to)) {
 					transactions.push({
 						...tx,
 						date: blockDate.toLocaleDateString(),
