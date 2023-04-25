63ae7d3b47a65361a72ccf8f82729bd56cd3241b
carlestorru <carlestorru@gmail.com>
2023-04-25 08:56:54 +0200

    add logo to header

--- client/src/layout/Header.jsx
+++ client/src/layout/Header.jsx
@@ -1,5 +1,7 @@
 import Menu from '../components/Icons/Menu';
 import { useLayoutMode } from '../context/LayoutContext';
+import Logo from '../assets/3pay_logo/4x/3pay_logo_gradient@4x.png';
+
 
 export default function Header() {
     const [, toggleSideBar] = useLayoutMode()
@@ -11,7 +13,7 @@ export default function Header() {
 	return (
 		<header className='flex items-center justify-between bg-white text-slate-600 dark:bg-slate-800 dark:text-slate-200 md:hidden'>
 			{/* Logo */}
-			<span className='ml-3 text-2xl font-extrabold'>ePay3</span>
+			<img className='h-9 w-16 ml-4' src={Logo} alt='3pay logo' />
 
 			<button
 				className='p-4 focus:bg-blue-300 dark:focus:bg-blue-500 focus:outline-none'
