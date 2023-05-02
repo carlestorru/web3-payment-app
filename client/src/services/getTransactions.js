8e02be03ea4536b4366f369b68697bc4b94f46e6
carlestorru <carlestorru@gmail.com>
2023-05-02 16:48:53 +0200

    add try catch

--- client/src/services/getTransactions.js
+++ client/src/services/getTransactions.js
@@ -1,17 +1,24 @@
 export default async function getTransactions(account, web3) {
 	// Makes an HTTP request to a local server to get the transactions associated with the specified account.
-	const response = await fetch(
-		`http://localhost:3001/api/v1/transactions/${account}`
-	);
-
-	// Checks if the response is successful (status code 200).
-	if (response.ok) {
-		// If the response is successful, gets the data from the response in JSON format.
-		const data = await response.json();
-		// Returns the obtained data.
-		return data;
-	} else {
-		console.log('from blockchain')
+	try {
+		const response = await fetch(
+			`http://localhost:3001/api/v1/transactions/${account}`
+		);
+		// Checks if the response is successful (status code 200).
+		if (response.ok) {
+			// If the response is successful, gets the data from the response in JSON format.
+			const data = await response.json();
+			// Returns the obtained data.
+			return data;
+		} else {
+			console.log('from blockchain');
+			// If the response is not successful, gets the transactions from the blockchain using the Web3 library.
+			const data = await getTransactionsFromBlockchain(account, web3);
+			// Returns the obtained data.
+			return data;
+		}
+	} catch (err) {
+		console.log('from blockchain');
 		// If the response is not successful, gets the transactions from the blockchain using the Web3 library.
 		const data = await getTransactionsFromBlockchain(account, web3);
 		// Returns the obtained data.
@@ -39,7 +46,7 @@ async function getTransactionsFromBlockchain(account, web3) {
 				// Get the information for the current transaction.
 				const tx = await web3.eth.getTransaction(txHash);
 				// Check if the transaction is related to the given account.
-				if ( /* tx.to !== null && */ (account === tx.from || account === tx.to)) {
+				if (/* tx.to !== null && */ account === tx.from || account === tx.to) {
 					// Add the transaction information to the transactions array.
 					transactions.push({
 						...tx,
